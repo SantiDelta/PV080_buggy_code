89ccefd109bd3e672d4279e720d29131e44fddc9
Adam Masár <118212471+SantiDelta@users.noreply.github.com>
2025-05-15 14:44:32 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -3,7 +3,7 @@ import os
 import yaml
 import flask
 
-app = flask.Flask(__name__)
+APP = flask.Flask(__name__)
 
 
 @app.route("/")
@@ -28,7 +28,7 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
  
-    try: 
+    try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    if choice == "1":
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
